a252a6a7d750e9825da59b36ef90d9a3ff5ab226
Md. Shariful Islam <sharif.floralimited@gmail.com>
2022-06-08 12:26:23 +0600

    initial commit

---
+++ PositivePayApi/BasicAuthenticationAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace PositivePayApi
+{
+    public class BasicAuthenticationAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            IEnumerable<string> accessKey;
+            if (actionContext.Request.Headers.Authorization == null || !actionContext.Request.Headers.TryGetValues("api_key", out accessKey) || !accessKey.Any(x => x == ConfigurationManager.AppSettings["api_key"]))
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            string decodedAuthenticationToken = Encoding.UTF8.GetString(Convert.FromBase64String(actionContext.Request.Headers.Authorization.Parameter));
+            string[] usernamePasswordArray = decodedAuthenticationToken.Split(':');
+            string uname = usernamePasswordArray[0];
+            string pass = usernamePasswordArray[1];
+            if (uname == ConfigurationManager.AppSettings["user_name"] && pass == ConfigurationManager.AppSettings["password"])
+            {
+                Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(uname), null);
+            }
+            else
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
---
+++ PositivePayApi/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace PositivePayApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        [BasicAuthentication]
+        public HttpResponseMessage Get()
+        {
+            string userName = Thread.CurrentPrincipal.Identity.Name;
+            if (userName == "")
+                return Request.CreateResponse(HttpStatusCode.Unauthorized);
+
+            return Request.CreateResponse(HttpStatusCode.OK, new { Employees = new List<object> { new { Name = "Test1", Age = 25 }, new { Name = "Test 2", Age = 30 } } });
+        }
+    }
+}
